3a5175ac4274f67fc7a71a11f0bbfd00d0b0d57e
Sharon Njogu <njogusharon22@gmail.com>
2023-08-10 10:55:22 +0300

    Fixing Errors

--- initial.js
+++ initial.js
@@ -1,19 +1,23 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navlinks = document.querySelectorAll('.nav-link')
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navlinks = document.querySelectorAll('.nav-link');
+    const btn = document.getElementById('btn');
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});  
+    navToggle.addEventListener('click', () => {
+        document.body.classList.toggle('nav-open');
+    });
 
-navlinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+    navlinks.forEach(link => {
+        link.addEventListener('click', () => {
+            document.body.classList.remove('nav-open');
+        });
+    });
 
-btn.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});  
+    btn.addEventListener('click', () => {
+        document.body.classList.toggle('nav-open');
+        
+    });
+});
 
 
 
